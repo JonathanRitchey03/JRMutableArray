3cc74d87e34bf2ba5afbff40c922ad53a5e56f09
Jonathan Ritchey <jonathan@kurbo.com>
2015-10-06 07:23:57 -0700

    factor out JRDrawKit

---
+++ JRMutableArray/Source/JRDrawKit.swift
@@ -0,0 +1,145 @@
+import UIKit
+
+class JRDrawKit: NSObject {
+
+    var currentArray : NSMutableArray!
+    var currentColor : UIColor!
+    var currentItemHeight : Double!
+    
+    override init() {
+        currentColor = UIColor.whiteColor()
+        currentItemHeight = 14
+        currentArray = []
+    }
+    
+    internal func drawIndices(xPos: Double) {
+        let paragraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
+        paragraphStyle.lineBreakMode = NSLineBreakMode.ByTruncatingTail
+        let textFontAttributes = [NSFontAttributeName: UIFont.systemFontOfSize(CGFloat(currentItemHeight-2)), NSForegroundColorAttributeName: currentColor, NSParagraphStyleAttributeName: paragraphStyle]
+        for i in 0..<currentArray.count {
+            let y = Double(i) * currentItemHeight
+            let indexString = String(i)
+            indexString.drawAtPoint(CGPointMake(CGFloat(xPos), CGFloat(y)), withAttributes: textFontAttributes)
+        }
+    }
+    
+    private func objectDescription(object:AnyObject?) -> String {
+        if object != nil {
+            let hasDescription = object!.respondsToSelector(Selector("description"))
+            return hasDescription ? object!.description : ""
+        }
+        return ""
+    }
+    
+    internal func drawObjectDescriptions(xPos:Double) {
+        let paragraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
+        paragraphStyle.lineBreakMode = NSLineBreakMode.ByTruncatingTail
+        let textFontAttributes = [NSFontAttributeName: UIFont.systemFontOfSize(CGFloat(currentItemHeight-2)), NSForegroundColorAttributeName: currentColor, NSParagraphStyleAttributeName: paragraphStyle]
+        for i in 0..<currentArray.count {
+            let y = Double(i) * currentItemHeight
+            let desc = objectDescription(currentArray.objectAtIndex(i))
+            desc.drawAtPoint(CGPointMake(CGFloat(xPos), CGFloat(y)), withAttributes: textFontAttributes)
+        }
+    }
+    
+    private func getDoubleForObject(object:AnyObject?)->Double {
+        if object is Double || object is Float || object is Int {
+            return object as! Double
+        }
+        return Double.NaN
+    }
+    
+    private func arrayHasNumber()->Bool {
+        for i in 0..<currentArray.count {
+            let object : AnyObject? = currentArray.objectAtIndex(i)
+            if object is Double || object is Float || object is Int {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func getRange() -> (Double,Double) {
+        var min : Double = Double.NaN
+        var max : Double = Double.NaN
+        if arrayHasNumber() {
+            for i in 0..<currentArray.count {
+                let object : AnyObject? = currentArray.objectAtIndex(i)
+                let d : Double = getDoubleForObject(object)
+                if d != Double.NaN && min.isNaN {
+                    min = d
+                    max = d
+                }
+                if ( d < min ) {
+                    min = d
+                }
+                if ( d > max ) {
+                    max = d
+                }
+            }
+        }
+        return (min,max)
+    }
+    
+    private func getPercentageInRange(d : Double, min : Double, max : Double) -> Double {
+        let delta = max - min
+        if delta != 0 {
+            let percentage = d / delta
+            return percentage
+        }
+        return 0
+    }
+    
+    internal func drawLine(x0:Double,y0:Double,x1:Double,y1:Double) {
+        let path = UIBezierPath()
+        path.moveToPoint(CGPointMake(CGFloat(x0), CGFloat(y0)))
+        path.addLineToPoint(CGPointMake(CGFloat(x1), CGFloat(y1)))
+        path.lineWidth = 0.5
+        currentColor.setStroke()
+        path.stroke()
+    }
+    
+    internal func drawLines(viewWidth: Double) {
+        for i in 0..<currentArray.count {
+            let y = Double(i) * currentItemHeight
+            drawLine(0, y0: y, x1: viewWidth, y1: y)
+        }
+    }
+    
+    internal func drawBarsIfNumbers(xPos:Double, viewWidth: Double) {
+        if arrayHasNumber() {
+            var min : Double = Double.NaN
+            var max : Double = Double.NaN
+            (min,max) = getRange()
+            if ( min > 0 ) {
+                min = 0
+            }
+            let width : Double = viewWidth - xPos
+            for i in 0..<currentArray.count {
+                let y = Double(i) * currentItemHeight
+                let barHeight : CGFloat = CGFloat(currentItemHeight - 2)
+                let object : AnyObject? = currentArray.objectAtIndex(i)
+                if object != nil {
+                    if object is Double || object is Float || object is Int {
+                        let d = getDoubleForObject(object)
+                        var x0Line : Double = xPos
+                        if ( min < 0 ) {
+                            x0Line = xPos + getPercentageInRange(-min, min: min, max: max) * width
+                        }
+                        var rectPath : UIBezierPath
+                        if ( d > 0 ) {
+                            let posBarWidth = getPercentageInRange(d, min: min, max: max) * width
+                            rectPath = UIBezierPath(rect: CGRectMake(CGFloat(x0Line),CGFloat(y+1),CGFloat(posBarWidth),barHeight))
+                        } else {
+                            let negBarWidth = getPercentageInRange(-d, min: min, max: max) * width
+                            rectPath = UIBezierPath(rect: CGRectMake(CGFloat(x0Line - negBarWidth),CGFloat(y+1),CGFloat(negBarWidth),barHeight))
+                        }
+                        UIColor.blueColor().setFill()
+                        rectPath.fill()
+                    }
+                }
+            }
+        }
+    }
+    
+}
--- JRMutableArray/Source/JRMutableArray.swift
+++ JRMutableArray/Source/JRMutableArray.swift
@@ -2,11 +2,9 @@ import Foundation
 import UIKit
 
 public class JRMutableArray : NSObject {
-    private let itemHeight = 12
-    private let viewWidth : CGFloat = 600
+
     private var storage : NSMutableArray
     private var queue : dispatch_queue_t
-    public var textColor : UIColor?
     
     override init() {
         storage = NSMutableArray()
@@ -45,152 +43,34 @@ public class JRMutableArray : NSObject {
     }
     
     func debugQuickLookObject() -> AnyObject? {
-        let size = CGSizeMake(CGFloat(viewWidth),CGFloat(self.storage.count*itemHeight))
+        let itemHeight : Double = 12
+        let viewWidth : Double = 600
+        let drawKit = JRDrawKit()
+        drawKit.currentArray = storage
+        drawKit.currentItemHeight = itemHeight
+        let size = CGSizeMake(CGFloat(viewWidth),CGFloat(Double(self.storage.count)*itemHeight))
         UIGraphicsBeginImageContextWithOptions(size, true, 0)
         UIColor.blackColor().setFill()
         UIRectFill(CGRectMake(0, 0, size.width, size.height))
-        let indexObjectDividerX : CGFloat = 40
-        let indexObjectDividerMarginX : CGFloat = 5
-        let objectGraphDividerX : CGFloat = 70
-        drawLines(UIColor.grayColor())
-        drawLine(UIColor.grayColor(), x0: indexObjectDividerX - indexObjectDividerMarginX, y0: 0,
-                                      x1: indexObjectDividerX - indexObjectDividerMarginX, y1: size.height)
-        drawLine(UIColor.grayColor(), x0: objectGraphDividerX, y0: 0, x1: objectGraphDividerX, y1: size.height)
-        drawBarsIfNumbers(objectGraphDividerX)
-        drawIndices(UIColor.whiteColor(), xPos:5)
-        drawObjectDescriptions(CGFloat(indexObjectDividerX), textColor: UIColor.greenColor())
+        let leftMarginX : Double = 5
+        let indexObjectDividerX : Double = 40
+        let indexObjectDividerMarginX : Double = 5
+        let objectGraphDividerX : Double = 70
+        drawKit.currentColor = UIColor.grayColor()
+        drawKit.drawLines(viewWidth)
+        drawKit.drawLine(indexObjectDividerX - indexObjectDividerMarginX, y0: 0,
+                         x1: indexObjectDividerX - indexObjectDividerMarginX, y1: Double(size.height))
+        drawKit.drawLine(objectGraphDividerX, y0: 0,
+                         x1: objectGraphDividerX, y1: Double(size.height))
+        drawKit.currentColor = UIColor.blueColor()
+        drawKit.drawBarsIfNumbers(objectGraphDividerX, viewWidth: viewWidth)
+        drawKit.currentColor = UIColor.whiteColor()
+        drawKit.drawIndices(leftMarginX)
+        drawKit.drawObjectDescriptions(indexObjectDividerX)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image
-    }
-    
-    private func drawIndices(textColor:UIColor, xPos:CGFloat) {
-        let paragraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
-        paragraphStyle.lineBreakMode = NSLineBreakMode.ByTruncatingTail
-        let textFontAttributes = [NSFontAttributeName: UIFont.systemFontOfSize(CGFloat(itemHeight-2)), NSForegroundColorAttributeName: textColor, NSParagraphStyleAttributeName: paragraphStyle]
-        for i in 0..<self.count() {
-            let y = i * itemHeight
-            let string : NSString? = self.storage.objectAtIndex(i).description
-            if string != nil {
-                let indexString = String(i)
-                indexString.drawAtPoint(CGPointMake(xPos, CGFloat(y)), withAttributes: textFontAttributes)
-            }
-        }
-    }
-
-    private func drawObjectDescriptions(xPos:CGFloat, textColor:UIColor) {
-        let paragraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
-        paragraphStyle.lineBreakMode = NSLineBreakMode.ByTruncatingTail
-        let textFontAttributes = [NSFontAttributeName: UIFont.systemFontOfSize(CGFloat(itemHeight-2)), NSForegroundColorAttributeName: textColor, NSParagraphStyleAttributeName: paragraphStyle]
-        for i in 0..<self.count() {
-            let y = i * itemHeight
-            let string : NSString? = self.storage.objectAtIndex(i).description
-            if string != nil {
-                string?.drawAtPoint(CGPointMake(xPos, CGFloat(y)), withAttributes: textFontAttributes)
-            }
-        }
-    }
-
-    private func getDoubleForObject(object:AnyObject?)->Double {
-        if object is Double || object is Float || object is Int {
-            return object as! Double
-        }
-        return Double.NaN
-    }
-    
-    private func arrayHasNumber()->Bool {
-        for i in 0..<self.count() {
-            let object : AnyObject? = self.storage.objectAtIndex(i)
-            if object is Double || object is Float || object is Int {
-                return true
-            }
-        }
-        return false
-    }
-    
-    private func getRange() -> (Double,Double) {
-        var min : Double = Double.NaN
-        var max : Double = Double.NaN
-        if arrayHasNumber() {
-            for i in 0..<self.count() {
-                let object : AnyObject? = self.storage.objectAtIndex(i)
-                let d : Double = getDoubleForObject(object)
-                if d != Double.NaN && min.isNaN {
-                    min = d
-                    max = d
-                }
-                if ( d < min ) {
-                    min = d
-                }
-                if ( d > max ) {
-                    max = d
-                }
-            }
-        }
-        return (min,max)
-    }
-    
-    private func getPercentageInRange(d : Double, min : Double, max : Double) -> CGFloat {
-        let delta = max - min
-        if delta != 0 {
-            let percentage = CGFloat(d / delta)
-            return percentage
-        }
-        return 0
-    }
-    
-    private func drawLine(color:UIColor,x0:CGFloat,y0:CGFloat,x1:CGFloat,y1:CGFloat) {
-        let path = UIBezierPath()
-        path.moveToPoint(CGPointMake(x0, y0))
-        path.addLineToPoint(CGPointMake(x1, y1))
-        path.lineWidth = 0.5
-        color.setStroke()
-        path.stroke()
-    }
-    
-    private func drawLines(color:UIColor) {
-        for i in 0..<self.count() {
-            let y = CGFloat(i * itemHeight)
-            drawLine(color, x0: 0, y0: y, x1: viewWidth, y1: y)
-        }
-    }
-    
-    private func drawBarsIfNumbers(xPos:CGFloat) {
-        if arrayHasNumber() {
-            var min : Double = Double.NaN
-            var max : Double = Double.NaN
-            (min,max) = getRange()
-            if ( min > 0 ) {
-                min = 0
-            }
-            let width : CGFloat = CGFloat(viewWidth - xPos)
-            for i in 0..<self.count() {
-                let y = i * itemHeight
-                let barHeight : CGFloat = CGFloat(itemHeight - 2)
-                let object : AnyObject? = self.storage.objectAtIndex(i)
-                if object != nil {
-                    if object is Double || object is Float || object is Int {
-                        let d = getDoubleForObject(object)
-                        var x0Line : CGFloat = xPos
-                        if ( min < 0 ) {
-                            x0Line = xPos + getPercentageInRange(-min, min: min, max: max) * width
-                        }
-                        var rectPath : UIBezierPath
-                        if ( d > 0 ) {
-                            let posBarWidth = getPercentageInRange(d, min: min, max: max) * width
-                            rectPath = UIBezierPath(rect: CGRectMake(CGFloat(x0Line),CGFloat(y+1),CGFloat(posBarWidth),barHeight))
-                        } else {
-                            let negBarWidth = getPercentageInRange(-d, min: min, max: max) * width
-                            rectPath = UIBezierPath(rect: CGRectMake(CGFloat(x0Line - negBarWidth),CGFloat(y+1),CGFloat(negBarWidth),barHeight))
-                        }
-                        UIColor.blueColor().setFill()
-                        rectPath.fill()
-                    }
-                }
-            }
-        }
-    }
-
+    }    
 }
 
 /****************	Mutable Array		****************/
